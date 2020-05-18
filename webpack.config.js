97576629084b16f42f01caf26064347135a84ba1
igor <starodubcevig@mail.ru>
2020-05-18 16:38:50 +0400

    config

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        main:"./src/main.js"
+    },
+    output: {
+        path: path.resolve(__dirname,"./dist"),
+        filename: "[name].js"
+    },
+    devServer: {
+       port:"8081",
+        historyApiFallback: true,
+        overlay:{
+            warnings:true,
+            errors:true
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[miniCssExtractPlugin.loader,"css-loader"]
+            },
+            {
+                test: /\.(wolf|wolf2|ttf|otf)$/,
+                use: [
+                    {
+                        loader:"file-loader",
+                        options: {
+                            outputPath: "fonts"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg|fif|svg)$/,
+                use:[
+                    {
+                        loader:"file-loader",
+                        options: {
+                            outputPath:"img"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/main.html"
+        }),
+        new miniCssExtractPlugin(),
+    ]
+}
